b8749dca648ce0ac5d109ae2076c933a3ee1c240
Leandro Martín Lafuente <llafuente@dc.uba.ar>
2024-01-28 18:29:40 -0300

    ejercicio 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,24 +7,43 @@ const Button = (props) => (
 )
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const promedio = (good-bad)/all
+  const positivas = (good*100)/all
+
+  const handleGood = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
 
-      <Button text="good" onClick={() => setGood(good + 1)} />
-      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
-      <Button text="bad" onClick={() => setBad(bad + 1)} />
+      <Button text="good" onClick={handleGood} />
+      <Button text="neutral" onClick={handleNeutral} />
+      <Button text="bad" onClick={handleBad} />
 
       <h1>statistics</h1>
       
       <div> good {good} </div>
       <div> neutral {neutral} </div>
       <div> bad {bad} </div>
+      <div> all {all} </div>
+      <div> average {promedio} </div>
+      <div> positive {positivas} % </div>
     </div>
   )
 }
